5918031d7dd3f7733ac0ed9e82196120fde86ba8
Aymeric <fanttoome2@hotmail.fr>
2022-06-25 14:55:11 +0200

    fixing indent and index reverse fail

--- hypecode/src/App.js
+++ hypecode/src/App.js
@@ -1,42 +1,41 @@
 import './App.css';
 import favicon from './assets/favicon.ico'
-import Helmet from "react-helmet"; 
+import Helmet from "react-helmet";
 import React, { useState } from 'react';
 
 
 function App() {
   const [tasks, setTasks] = useState([])
-  const [input, setInput]= useState("")
+  const [input, setInput] = useState("")
 
-const deleteTask = index => {
-  const updatedTasks = [...tasks]
-  updatedTasks.splice(index, 1)
-  setTasks([...updatedTasks])
-}
-
-const completeTask = e => {
-  if (e.target.style.backgroundColor === 'grey') {
-    e.target.style.backgroundColor = ""
+  const deleteTask = index => {
+    const updatedTasks = [...tasks]
+    updatedTasks.splice(index, 1)
+    setTasks([...updatedTasks])
   }
-  else e.target.style.backgroundColor='grey'
-}
 
-const saveInput = e => {
-  setInput(e.target.value);
-};
-
-const handleTask = () => {
-  const newTask = tasks
-  newTask.push(input)
-  setTasks([...newTask])
-}
-
-const handleKeyUp = (event) => {
-  console.log(event.key)
-  if(event.key === 'Enter'){
-    handleTask()
+  const completeTask = e => {
+    if (e.target.style.backgroundColor === 'grey') {
+      e.target.style.backgroundColor = ""
+    }
+    else e.target.style.backgroundColor = 'grey'
   }
-}
+
+  const saveInput = e => {
+    setInput(e.target.value);
+  };
+
+  const handleTask = () => {
+    const newTask = tasks
+    newTask.push(input)
+    setTasks([...newTask])
+  }
+
+  const handleKeyUp = (event) => {
+    if (event.key === 'Enter') {
+      handleTask()
+    }
+  }
 
   return (
     <div className="App">
@@ -46,21 +45,23 @@ const handleKeyUp = (event) => {
 
       <div className='app-addTask'>
         <input className='app-input' placeholder='Your task' type="text" onKeyUp={e => handleKeyUp(e)} onChange={e => saveInput(e)} />
-        <button onClick={handleTask}>Add Item</button>
+        <button onClick={handleTask}>Add your task !</button>
       </div>
 
-
-      {tasks.concat().reverse().map((value, index) => (
-        <div className="app-tasks">
-          <div className='app-task'>
-            {value}
+      {/* {tasks.concat().sort((a, b) => b - a).reverse().map((value, index) => ( */}
+      <div className='app-taskBoard'>
+        {tasks.map((value, index) => (
+          <div className="app-tasks">
+            <div className='app-task'>
+              {value}
+            </div>
+            <div className='app-buttons'>
+              <button onClick={completeTask}>Complete</button>
+              <button onClick={() => deleteTask(index)}>Delete</button>
+            </div>
           </div>
-          <div className='app-buttons'>
-            <button onClick={completeTask}>Complete</button>
-            <button onClick={() => deleteTask(index)}>Delete</button>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
